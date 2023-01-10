ab65fef74de8f1cce1fa51c5f8142e1277c7c391
4o4E <869951226@qq.com>
2023-01-10 13:11:25 +0800

    分离ConfigData到单独文件, 设置yaml不使用严格模式

--- src/main/kotlin/config/Config.kt
+++ src/main/kotlin/config/Config.kt
@@ -1,7 +1,7 @@
 package top.e404.eclean.config
 
 import com.charleskorn.kaml.Yaml
-import kotlinx.serialization.Serializable
+import com.charleskorn.kaml.YamlConfiguration
 import top.e404.eclean.PL
 import top.e404.eplugin.config.ESerializationConfig
 import top.e404.eplugin.config.JarConfig
@@ -11,16 +11,5 @@ object Config : ESerializationConfig<ConfigData>(
     path = "config.yml",
     default = JarConfig(PL, "config.yml"),
     serializer = ConfigData.serializer(),
-    format = Yaml.default
-)
-
-@Serializable
-data class ConfigData(
-    var debug: Boolean,
-    var update: Boolean,
-    val duration: Long,
-    val message: Map<Long, String>,
-    val living: LivingConfig,
-    val drop: DropConfig,
-    val chunk: ChunkConfig,
+    format = Yaml(configuration = YamlConfiguration(strictMode = false))
 )
