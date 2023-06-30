83dd1ef27d488f72cff307c3e60be1e0dc1c5b5f
4o4E <869951226@qq.com>
2023-06-30 16:28:30 +0800

    优化reload流程

--- src/main/kotlin/command/Reload.kt
+++ src/main/kotlin/command/Reload.kt
@@ -5,7 +5,6 @@ import top.e404.eclean.PL
 import top.e404.eclean.clean.Clean
 import top.e404.eclean.config.Config
 import top.e404.eclean.config.Lang
-import top.e404.eplugin.EPlugin.Companion.color
 import top.e404.eplugin.command.ECommand
 
 object Reload : ECommand(
@@ -15,40 +14,16 @@ object Reload : ECommand(
     false,
     "eclean.admin"
 ) {
-    private val s1 = "&c配置文件`config.yml`格式错误".color()
-    private val s2 = "&c计划清理任务时出现异常".color()
-    override val usage: String
-        get() = Lang["command.usage.reload"]
+    override val usage get() = Lang["command.usage.reload"]
 
     override fun onCommand(sender: CommandSender, args: Array<out String>) {
         plugin.runTaskAsync {
-            try {
-                Config.load(sender)
-            } catch (e: Exception) {
-                plugin.sendAndWarn(
-                    sender,
-                    Lang[
-                        "message.invalid_config",
-                        "file" to "config.yml"
-                    ],
-                    e
-                )
-                return@runTaskAsync
-            }
-            try {
+            Lang.load(sender)
+            Config.load(sender)
+            plugin.runTask {
                 Clean.schedule()
-            } catch (e: Exception) {
-                plugin.sendAndWarn(
-                    sender,
-                    Lang[
-                        "message.invalid_config",
-                        "file" to "config.yml"
-                    ],
-                    e
-                )
-                return@runTaskAsync
+                plugin.sendMsgWithPrefix(sender, Lang["command.reload_done"])
             }
-            plugin.sendMsgWithPrefix(sender, Lang["command.reload_done"])
         }
     }
 }
