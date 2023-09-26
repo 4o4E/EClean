737344147814add1e3aae47303cae76d34314901
4o4E <869951226@qq.com>
2023-09-27 03:30:43 +0800

    更新版本和依赖, 引入MockBukkit

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,19 @@ plugins {
 }
 
 group = "top.e404"
-version = "1.16.1"
-val epluginVer = "1.1.0"
+version = "1.17.0"
+val epluginVer = "1.2.0"
 
 fun eplugin(module: String, version: String = epluginVer) = "top.e404:eplugin-$module:$version"
 
 repositories {
     mavenLocal()
-    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven(url = "https://oss.sonatype.org/content/groups/public/")
+    // papermc
+    maven("https://repo.papermc.io/repository/maven-public/")
+    // spigot
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    // sonatype
+    maven("https://oss.sonatype.org/content/groups/public/")
     // placeholderAPI
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     mavenCentral()
@@ -33,34 +37,41 @@ dependencies {
     compileOnly("me.clip:placeholderapi:2.11.1")
     // Bstats
     implementation("org.bstats:bstats-bukkit:3.0.0")
+
+    // mock bukkit
+    testImplementation(kotlin("test", "1.8.21"))
+    testImplementation("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.21.1")
+    testImplementation("org.slf4j:slf4j-simple:2.0.7")
+    testImplementation("net.kyori:adventure-text-serializer-legacy:4.14.0")
 }
 
 tasks {
+    build {
+        finalizedBy(shadowJar)
+    }
+
     shadowJar {
-        archiveFileName.set("${project.name}-${project.version}.jar")
+        val archiveName = "${project.name}-${project.version}.jar"
+        archiveFileName.set(archiveName)
+
         relocate("org.bstats", "top.e404.eclean.relocate.bstats")
         relocate("kotlin", "top.e404.eclean.relocate.kotlin")
         relocate("top.e404.eplugin", "top.e404.eclean.relocate.eplugin")
         relocate("com.charleskorn.kaml", "top.e404.eclean.relocate.kaml")
         exclude("META-INF/**")
-        doFirst {
-            for (file in File("jar").listFiles() ?: arrayOf()) {
-                println("正在删除`${file.name}`")
-                file.delete()
-            }
-        }
 
         doLast {
-            File("jar").mkdirs()
-            for (file in File("build/libs").listFiles() ?: arrayOf()) {
-                println("正在复制`${file.name}`")
-                file.copyTo(File("jar/${file.name}"), true)
-            }
+            val archiveFile = archiveFile.get().asFile
+            println(archiveFile.parentFile.absolutePath)
+            println(archiveFile.absolutePath)
         }
     }
 
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
+
+        dependsOn(clean)
     }
 
     processResources {
@@ -68,5 +79,10 @@ tasks {
         filesMatching("plugin.yml") {
             expand("version" to project.version)
         }
+    }
+
+    test {
+        useJUnitPlatform()
+        this.systemProperties["eclean.debug"] = true
     }
 }
